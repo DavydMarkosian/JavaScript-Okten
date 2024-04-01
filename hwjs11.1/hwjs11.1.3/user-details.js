ef7869277fcb818e8b33c7b0df20d548a7feb5b7
DavydMarkosian <markdaiv97@gmail.com>
2024-04-01 21:49:47 +0300

    11 hw js url fetch

--- hwjs11.1/hwjs11.1.3/user-details.js
+++ hwjs11.1/hwjs11.1.3/user-details.js
@@ -10,73 +10,164 @@
 
 let url = new URL(location.href)
 let id = url.searchParams.get('id');
-console.log(id)
 
 fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then(response => response.json())
     .then(value => {
-        let div = document.createElement('div')
-        document.body.appendChild(div)
+        let wrap = document.createElement('div')
+        document.body.appendChild(wrap)
+        console.log(value)
+        console.log(wrap)
 
-        let h3Name = document.createElement('h3')
-        let h3UserName = document.createElement('h3')
-        let pId = document.createElement('p')
-        let pEmail = document.createElement('p')
-        let pPhone = document.createElement('p')
-        let pWebsite = document.createElement('p')
-        let ulAd = document.createElement('ul')
-        let ulCom = document.createElement('ul')
+        function createListRecursion(value, wrap) {
+            let ul = document.createElement('ul')
 
-        h3Name.innerText = ` ${value.name}`
-        h3UserName.innerText = `username: ${value.username}`
-        pId.innerText = `id: ${value.id}`
-        pEmail.innerText = `${value.email}`
-        pPhone.innerText = `${value.phone}`
-        pWebsite.innerText = `${value.website}`
-        ulAd.innerText = `Address:`
-        ulCom.innerText = `Company:`
+            console.log(wrap);
 
-        let address = value.address
-        let {city, street, suite, zipcode, geo} = address
-        let arrAd = [city, street, suite, zipcode, geo]
+            console.log(ul)
+            wrap.appendChild(ul)
 
-        for (const arrElement of arrAd) {
-            let li = document.createElement('li')
-            li.innerText = arrElement
-            ulAd.appendChild(li)
+            for (const key in value) {
 
-            if (typeof arrElement == 'object') {
-                li.innerText = 'Geo:'
+                let li = document.createElement('li')
+                ul.appendChild(li)
+                li.innerText = `${key} : ${value[key]} `
 
-                let {lat, lng} = arrElement
-                let arrGeo = [lat, lng]
-                for (const arrGeoElement of arrGeo) {
-                    let ulInner = document.createElement('ul')
-                    ulAd.appendChild(ulInner)
-
-                    let liInner = document.createElement('li')
-                    ulInner.appendChild(liInner)
-                    liInner.innerText = arrGeoElement
+                if (typeof value[key] === 'object' && value[key] !== null) {
+                    li.innerText = `${key}: `
+                    createListRecursion(value[key], li)
                 }
-
             }
-
         }
-        div.append(h3Name, h3UserName, pId, pEmail, pPhone, pWebsite, ulAd)
-        let company = value.company
-        let {bs, catchPhrase, name} = company
-        let arr = [bs, catchPhrase, name]
 
-        for (const arrElement of arr) {
-            div.appendChild(ulCom)
-
-            let li = document.createElement('li')
-            li.innerText = arrElement
-            ulCom.appendChild(li)
-        }
+        createListRecursion(value, wrap)
     })
 
 
+        //
+        // let h3Name = document.createElement('h3')
+        // let h3UserName = document.createElement('h3')
+        // let pId = document.createElement('p')
+        // let pEmail = document.createElement('p')
+        // let pPhone = document.createElement('p')
+        // let pWebsite = document.createElement('p')
+        // let ulAd = document.createElement('ul')
+        // let ulCom = document.createElement('ul')
+        //
+        // h3Name.innerText = ` ${value.name}`
+        // h3UserName.innerText = `username: ${value.username}`
+        // pId.innerText = `id: ${value.id}`
+        // pEmail.innerText = `${value.email}`
+        // pPhone.innerText = `${value.phone}`
+        // pWebsite.innerText = `${value.website}`
+        // ulAd.innerText = `Address:`
+        // ulCom.innerText = `Company:`
+
+        // let address = value.address
+        // let {city, street, suite, zipcode, geo} = address
+        // let arrAd = [city, street, suite, zipcode, geo]
+        //
+        // for (const arrElement of arrAd) {
+        //     let li = document.createElement('li')
+        //     li.innerText = arrElement
+        //     ulAd.appendChild(li)
+        //
+        //     if (typeof arrElement == 'object') {
+        //         li.innerText = 'Geo:'
+        //
+        //         let {lat, lng} = arrElement
+        //         let arrGeo = [lat, lng]
+        //         for (const arrGeoElement of arrGeo) {
+        //             let ulInner = document.createElement('ul')
+        //             ulAd.appendChild(ulInner)
+        //
+        //             let liInner = document.createElement('li')
+        //             ulInner.appendChild(liInner)
+        //             liInner.innerText = arrGeoElement
+        //         }
+        //
+        //     }
+        //
+        // }
+        // div.append(h3Name, h3UserName, pId, pEmail, pPhone, pWebsite, ulAd)
+        // let company = value.company
+        // let {bs, catchPhrase, name} = company
+        // let arr = [bs, catchPhrase, name]
+        //
+        // for (const arrElement of arr) {
+        //     div.appendChild(ulCom)
+        //
+        //     let li = document.createElement('li')
+        //     li.innerText = arrElement
+        //     ulCom.appendChild(li)
+        // }
+
+
+
+
+//==============================================user-details mini project
+//===================================================================================================================
+// let h3Name = document.createElement('h3')
+// let h3UserName = document.createElement('h3')
+// let pId = document.createElement('p')
+// let pEmail = document.createElement('p')
+// let pPhone = document.createElement('p')
+// let pWebsite = document.createElement('p')
+// let ulAd = document.createElement('ul')
+// let ulCom = document.createElement('ul')
+//
+// h3Name.innerText = `name:  ${value.name}`
+// h3UserName.innerText = `username: ${value.username}`
+// pId.innerText = `id: ${value.id}`
+// pEmail.innerText = `email:  ${value.email}`
+// pPhone.innerText = `phone: ${value.phone}`
+// pWebsite.innerText = `website: ${value.website}`
+// ulAd.innerText = `Address:`
+// ulCom.innerText = `Company:`
+//
+// let address = value.address
+// let {city, street, suite, zipcode, geo} = address
+// let arrAd = [{city}, {street}, {suite}, {zipcode}, {geo}]
+//
+// for (const arrElement of arrAd) {
+//     let li = document.createElement('li')
+//     li.innerText = `${Object.keys(arrElement)}: ${Object.values(arrElement)}`
+//     ulAd.appendChild(li)
+//
+//     if (Object.keys(arrElement) == 'geo') {
+//         let {lat, lng} = geo
+//         let arrGeo = [{lat}, {lng}]
+//
+//         let ulInner = document.createElement('ul')
+//         ulAd.appendChild(ulInner)
+//
+//         for (const arrGeoElement of arrGeo) {
+//             li.innerText = 'geo:'
+//
+//             let liInner = document.createElement('li')
+//             ulInner.appendChild(liInner)
+//
+//             liInner.innerText = `${Object.keys(arrGeoElement)}: ${Object.values(arrGeoElement)} `
+//
+//         }
+//
+//     }
+//
+// }
+//
+// div.append(h3Name, h3UserName, pId, pEmail, pPhone, pWebsite, ulAd)
+//
+// let company = value.company
+// let {bs, catchPhrase, name} = company
+// let arr = [{bs}, {catchPhrase}, {name}]
+//
+// for (const arrElement of arr) {
+//     div.appendChild(ulCom)
+//
+//     let li = document.createElement('li')
+//     li.innerText = `${Object.keys(arrElement)}: ${Object.values(arrElement)}`
+//     ulCom.appendChild(li)
+// }
 
 
 
